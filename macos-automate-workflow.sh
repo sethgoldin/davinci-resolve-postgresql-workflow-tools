06d828fde121e8db11aa7bf0a512af4947f47098
sethgoldin <sethgoldin@gmail.com>
2018-03-23 15:19:57 -0400

    Add files via upload

--- macos-automate-workflow.sh
+++ macos-automate-workflow.sh
@@ -4,23 +4,51 @@
 read -p "Enter the name of your DaVinci Resolve PostgreSQL database: " dbname
 
 # Let's allow the user to confirm that what they've typed in is correct:
-read -p "Did you enter the name of the database correctly? (y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1 
+echo "You entered: $dbname"
+read -p "Is that correct? Enter y or n: " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1 
 
-# Now "$dbname" will work as a variable in subsequent paths
+# Now "$dbname" will work as a variable in subsequent paths.
 
 # Let's prompt the user for the "backup directory," which is where the backups from pg_dump will go:
 read -p "Into which directory should the database backups go? " backupDirectory
 
 # Let's also allow the user to confirm that what they've typed in for the backup directory is correct:
-read -p "Did you enter the path of the backup directory correctly? (y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+echo "You entered: $backupDirectory"
+read -p "Is that correct? Enter y or n: " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 # Now $backupDirectory will be the folder loaded into the shell script where the backups are going to go
+
+# Let's allow the user to specify how often to backup the database.
+# Let's also provide a handy list of suggested time intervals.
+echo "Handy list of suggested time intervals:"
+echo "     3600 seconds = 01 hour"
+echo "    10800 seconds = 03 hours"
+echo "    21600 seconds = 06 hours"
+echo "    43200 seconds = 12 hours"
+echo "    86400 seconds = 24 hours"
+read -p "How often would you like to backup the database, in seconds? " backupFrequency
+
+# Let's have the user confirm that they entered the right backup frequency:
+read -p "You entered that you want to backup your database every $backupFrequency seconds. Is that correct? Enter y or n: " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+# Let's allow the user to specify how often to optimize the database.
+# Let's also provide a handy list of suggested time intervals.
+echo "Handy list of suggested time intervals:"
+echo "     3600 seconds = 01 hour"
+echo "    10800 seconds = 03 hours"
+echo "    21600 seconds = 06 hours"
+echo "    43200 seconds = 12 hours"
+echo "    86400 seconds = 24 hours"
+read -p "How often would you like to optimize the database, in seconds? " optimizeFrequency
+
+# Let's have the user confirm that they entered the correct optimizing frequency:
+read -p "You entered that you want to optimize your database every $optimizeFrequency seconds. Is that correct? Enter y or n: " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 # Let's check if a ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools folder exists, and if it doesn't, let's create one:
 mkdir -p ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools
 
 # Let's also check to see if there are separate directories for "backup" and "optimize" scripts, and if they don't exist, let's create them.
-# We're making separate directories for the different kinds of scripts just to keep everything straight.
+# We're making separate directories for the different kinds of scripts just to keep everything clean and organized.
 
 mkdir -p ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/backup
 mkdir -p ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/optimize
@@ -33,7 +61,7 @@ chmod -R 755 ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/backup
 chmod -R 755 ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/optimize
 chmod -R 755 ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/logs
 
-# With those folders created with the correct permissions, let's go ahead and create the two different shell scripts that will be referenced by the launchd XML plist files.
+# With those folders created with the correct permissions, let's go ahead and create the two different shell scripts that will be executed by the launchd XML files.
 
 # First, let's create the "backup" shell script:
 touch ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/backup/backup-"$dbname".sh
@@ -42,7 +70,7 @@ touch ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/backup/backup-"$dbname".sh
 cat << EOF > ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/backup/backup-"$dbname".sh
 #!/bin/bash
 # Let's perform the backup and log to the monthly backup if the backup is successful.
-/Library/PostgreSQL/9.5/pgAdmin3.app/Contents/SharedSupport/pg_dump --host localhost --username postgres $dbname --blobs --file $backupDirectory/${dbname}_\$(date "+%Y_%m_%d_%H_%M").backup --format=custom --verbose --no-password && \
+/Library/PostgreSQL/9.5/pgAdmin3.app/Contents/SharedSupport/pg_dump --host localhost --username postgres $dbname --blobs --file $backupDirectory/${dbname}_\$(date "+%Y_%m_%d_%H_%M").backup --format=custom --verbose --no-password && \\
 echo "${dbname} was backed up at \$(date "+%Y_%m_%d_%H_%M")." >> ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/logs/logs-\$(date "+%Y_%m").log
 EOF
 
@@ -59,8 +87,8 @@ touch ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/optimize/optimize-"$dbname".sh
 cat << EOF > ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/optimize/optimize-"$dbname".sh
 #!/bin/bash
 # Let's optimize the database and log to the monthly backup if the backup is successful.
-/Library/PostgreSQL/9.5/bin/reindexdb --host localhost --username postgres $dbname --no-password --echo && \
-/Library/PostgreSQL/9.5/bin/vacuumdb --analyze --host localhost --username postgres $dbname --verbose --no-password && \
+/Library/PostgreSQL/9.5/bin/reindexdb --host localhost --username postgres $dbname --no-password --echo && \\
+/Library/PostgreSQL/9.5/bin/vacuumdb --analyze --host localhost --username postgres $dbname --verbose --no-password && \\
 echo "${dbname} was optimized at \$(date "+%Y_%m_%d_%H_%M")." >> ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/logs/logs-\$(date "+%Y_%m").log
 EOF
 
@@ -82,7 +110,7 @@ cat << EOF > ~/Library/LaunchAgents/backup-"$dbname".plist
     <key>Program</key>
         <string>$HOME/DaVinci-Resolve-PostgreSQL-Workflow-Tools/backup/backup-$dbname.sh</string>
     <key>StartInterval</key>
-    <integer>60</integer>
+    <integer>$backupFrequency</integer>
 </dict>
 </plist>
 EOF
@@ -99,7 +127,7 @@ cat << EOF > ~/Library/LaunchAgents/optimize-"$dbname".plist
     <key>Program</key>
         <string>$HOME/DaVinci-Resolve-PostgreSQL-Workflow-Tools/optimize/optimize-$dbname.sh</string>
     <key>StartInterval</key>
-    <integer>180</integer>
+    <integer>$optimizeFrequency</integer>
 </dict>
 </plist>
 EOF
@@ -118,3 +146,6 @@ launchctl start com.resolve.backup.${dbname}
 # Lastly, let's load and start the optimize agent.
 launchctl load ~/Library/LaunchAgents/optimize-${dbname}.plist
 launchctl start com.resolve.optimize.${dbname}
+
+echo "Congratulations, $dbname will be backed up every $backupFrequency seconds and optimized every $optimizeFrequency seconds."
+echo "You can check to make sure that everything is being backed up and optimized properly by looking at the log files in: ~/DaVinci-Resolve-PostgreSQL-Workflow-Tools/logs"
