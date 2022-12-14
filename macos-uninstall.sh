a66db94b6c6c27ce2b3c8ac265b4f93b36e82a5c
sethgoldin <4632977+sethgoldin@users.noreply.github.com>
2022-12-14 00:48:58 -0600

    Update macos-uninstall.sh
    Updated to match paths for `macos-install.sh`.

--- macos-uninstall.sh
+++ macos-uninstall.sh
@@ -11,28 +11,28 @@ read -p "Is that correct? Enter y or no: " confirm && [[ $confirm == [yY] || $co
 launchctl stop com.resolve.backup.$dbname
 
 # unload backup plist file
-launchctl unload $HOME/Library/LaunchAgents/backup-$dbname.plist
+launchctl unload /Library/LaunchDaemons/backup-$dbname.plist
 
 # stop optimize launchd job
 launchctl stop com.resolve.optimize.$dbname
 
 # unload optimize plist file
-launchctl unload $HOME/Library/LaunchAgents/optimize-$dbname.plist
+launchctl unload /Library/LaunchDaemons/optimize-$dbname.plist
 
 # remove backup launchd plist file
-rm $HOME/Library/LaunchAgents/backup-$dbname.plist
+rm /Library/LaunchDaemons/backup-$dbname.plist
 
 # remove optimize launchd plist file
-rm $HOME/Library/LaunchAgents/optimize-$dbname.plist
+rm /Library/LaunchDaemons/optimize-$dbname.plist
 
 # remove backup shell script
-rm $HOME/DaVinci-Resolve-PostgreSQL-Workflow-Tools/backup/backup-$dbname.sh
+rm /Users/Shared/DaVinci-Resolve-PostgreSQL-Workflow-Tools/backup/backup-$dbname.sh
 
 # remove optimize shell script
-rm $HOME/DaVinci-Resolve-PostgreSQL-Workflow-Tools/optimize/optimize-$dbname.sh
+rm /Users/Shared/DaVinci-Resolve-PostgreSQL-Workflow-Tools/optimize/optimize-$dbname.sh
 
 # log to monthly log file that $dbname has been uninstalled. $dbname will no longer be backed up or optimized
-echo "Backup and optimize tools for $dbname were uninstalled at $(date "+%Y_%m_%d_%H_%M"). $dbname will no longer be backed up or optimized." >> $HOME/DaVinci-Resolve-PostgreSQL-Workflow-Tools/logs/logs-$(date "+%Y_%m").log
+echo "Backup and optimize tools for $dbname were uninstalled at $(date "+%Y_%m_%d_%H_%M"). $dbname will no longer be backed up or optimized." >> /Users/Shared/DaVinci-Resolve-PostgreSQL-Workflow-Tools/logs/logs-$(date "+%Y_%m").log
 
 # send message to user in command-line program to inform them of the same
 echo "Backup and optimize tools for ${dbname} were uninstalled. Have a great day!"
